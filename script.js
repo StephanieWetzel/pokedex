1acbc2608dcf39900c5b0957a4ca2263018656fe
Stephanie Wetzel <st.wetzel96@hotmail.com>
2023-09-07 23:19:51 +0200

    added type-specific colors, main-Content style

--- script.js
+++ script.js
@@ -1,34 +1,80 @@
 let currentPokemon;
-let allPokemon; // Array mit 25 Pokemon
-let loadedPokemon = [];
+let allPokemon; // Array - only pokemon names
+let loadedPokemon = []; // 
+let pokemon; // loadedPokemon[i];
+let pokemonType1;
+let pokemonType2;
+
+const backgroundColours = {
+    normal: '#A8A878',
+    fire: '#F08030',
+    water: '#6890F0',
+    electric: '#F8D030',
+    grass: '#78C850',
+    ice: '#98D8D8',
+    fighting: '#C03028',
+    poison: '#A040A0',
+    ground: '#E0C068',
+    psychic: '#F85888',
+    bug: '#A8B820',
+    rock: '#B8A038',
+    ghost: '#705898',
+    dragon: '#7038F8',
+    dark: '#705848',
+    steel: '#B8B8D0',
+    fairy: '#EE99AC',
+};
+
+const typeColours = {
+    normal: '#C6C6A7',
+    fire: '#F5AC78',
+    water: '#9DB7F5',
+    electric: '#FAE078',
+    grass: '#A7DB8D',
+    ice: '#BCE6E6',
+    fighting: '#D67873',
+    poison: '#C183C1',
+    ground: '#EBD69D',
+    flying: '#A890F0',
+    psychic: '#FA92B2',
+    bug: '#C6D16E',
+    rock: '#D1C17D',
+    ghost: '#A292BC',
+    dragon: '#A27DFA',
+    dark: '#A29288',
+    steel: '#D1D1E0',
+    fairy: '#F4BDC9',
+};
 
 
 async function init() {
     await loadPokemon();
-    renderPokemon();
-}
-
-
-function renderPokemon() {
-    for (let i = 0; i < loadedPokemon.length; i++) {
-        const pokemon = loadedPokemon[i];
-        document.getElementById('mainContent').innerHTML += `
-        <div>${pokemon['name']}</div>
-        `;
-    }
+    console.log(loadedPokemon);
+    console.log(pokemon);
 }
 
 
 async function loadPokemon() {
     await fetchAllPokemon();
     for (let i = 0; i < allPokemon['results'].length; i++) {
-        await fetchCurrentPokemon(allPokemon['results'][i]['url']);
+        await fetchCurrentPokemon(allPokemon['results'][i]['url']); // request für einzelnes Pokemon zusammengestellt
     }
-    console.log(loadedPokemon);
+    renderPokemonCards();
 }
 
 
-async function fetchCurrentPokemon(url) {
+function renderPokemonCards() {
+    for (let i = 0; i < loadedPokemon.length; i++) {
+        pokemon = loadedPokemon[i];
+        getPokemonTypes();
+        document.getElementById('mainContent').innerHTML += pokemonThumbnail(i);
+        getPokemonBackgroundColors(i);
+        getPokemonTypeColors(i);
+    }
+}
+
+
+async function fetchCurrentPokemon(url) { // url aus Funktion 'loadPokemon() wird eingesetzt'
     let response = await fetch(url);
     currentPokemon = await response.json();
     loadedPokemon.push(currentPokemon);
@@ -36,13 +82,45 @@ async function fetchCurrentPokemon(url) {
 
 
 async function fetchAllPokemon() { // Array mit 25 Pokemon
-    let url = 'https://pokeapi.co/api/v2/pokemon?limit=25&offset=0';
+    let url = 'https://pokeapi.co/api/v2/pokemon?limit=50&offset=0';
     let response = await fetch(url);
     allPokemon = await response.json();
 }
 
 
+function getPokemonTypes() {
+    pokemonType1 = '';
+    pokemonType2 = '';
+    // empty String if there´s no type
 
+    if (pokemon['types'].length > 0) { // first type
+        pokemonType1 = pokemon['types'][0]['type']['name'];
+    }
+
+    if (pokemon['types'].length > 1) { // second type
+        pokemonType2 = pokemon['types'][1]['type']['name'];
+    }
+}
+
+
+function getPokemonBackgroundColors(i) {
+    let pokemonCard = document.getElementById(`pokemonCard${i}`);
+    let backgroundColor = backgroundColours[pokemonType1]; // pokemonType1 == backgroundColours[i]
+
+    pokemonCard.style.backgroundColor = backgroundColor;
+}
+
+
+function getPokemonTypeColors(i) {
+    let firstPokemonType = document.getElementById(`firstPokemonType${i}`);
+    let secondPokemonType = document.getElementById(`secondPokemonType${i}`);
+
+    let typeColor1 = typeColours[pokemonType1];
+    let typeColor2 = typeColours[pokemonType2];
+
+    firstPokemonType.style.backgroundColor = typeColor1;
+    secondPokemonType.style.backgroundColor = typeColor2;
+}
 
 
 
@@ -63,12 +141,4 @@ async function fetchAllPokemon() { // Array mit 25 Pokemon
 //     document.getElementById('pokemonWeight').innerHTML = currentPokemon['weight'];
 //     document.getElementById('pokemonAbility1').innerHTML = currentPokemon['abilities'][0]['ability']['name'];
 //     document.getElementById('pokemonAbility2').innerHTML = currentPokemon['abilities'][1]['ability']['name'];
-// }
-
-
-// function renderAllPokemon() {
-//     for (let i = 0; i < allPokemon['results'].length; i++) {
-//         const onePokemon = allPokemon['results'][i];
-//         document.getElementById('mainContent').innerHTML += allPokemonTemplate(onePokemon); // templates.js
-//     }
 // }
--- templates.js
+++ templates.js
@@ -1,12 +1,31 @@
+function pokemonThumbnail(i) {
+    return /*html*/`
+    <div onclick="" class="pokemonCard" id="pokemonCard${i}">
+        <div class="spaceBetween">
+            <h2 class="capitalLetters marginBlockStart0">${pokemon['name']}</h2>
+            <span>#${pokemon['id'].toString().padStart(3, "0")}</span>
+        </div>
+        <div class="typesAndImageContainer">
+            <div class="typesContainer">
+                <span id="firstPokemonType${i}">${pokemonType1}</span>
+                <span id="secondPokemonType${i}">${pokemonType2}</span>
+            </div>
+            <div class="pokemonImageContainer"><img src="${pokemon['sprites']['other']['official-artwork']['front_default']}"></div>
+        </div>
+    </div >
+        `;
+}
+
+
 // function currentPokemonTemplate() {
 //     return `
 //     <div id="">                                  >> NOCH ID EINFÜGEN <<
 //         <div class="fullWidth">
-//             <div class="symbolsContainer">
+//             <div class="spaceBetween">
 //                 <img class="invertWhite" src="./img/back.svg" alt="Pfeil zurück">
 //                 <img class="invertWhite" src="./img/heart.svg" alt="Herz">
 //             </div>
-//             <div class="nameContainer">
+//             <div class="spaceBetween">
 //                 <h1 class="capitalLetters" id="pokemonName"></h1>
 //                 <span id="pokemonId"></span>
 //             </div>
@@ -41,66 +60,6 @@
 //             </div>
 
 //             <h2>Breeding</h2>
-//         </div>
-//     </div>
-//     `;
-// }
-
-
-// function currentPokemonTemplate() {
-//     return `
-//     <div onclick="" class="pokemonCard">
-//         <h2 class="capitalLetters">${currentPokemon['name']}</h2>
-//         <span id="pokemonId">'#' + ${currentPokemon['id']}</span>
-//     </div>
-//     `;
-// }
-
-
-// function allPokemonTemplate(pokemonData) {
-//     return `
-//     <div class="pokemonCard">
-//         <div id="">                              >> NOCH ID EINFÜGEN <<
-//             <div class="fullWidth">
-//                 <div class="symbolsContainer">
-//                     <img class="invertWhite" src="./img/back.svg" alt="Pfeil zurück">
-//                     <img class="invertWhite" src="./img/heart.svg" alt="Herz">
-//                 </div>
-//                 <div class="nameContainer">
-//                     <h1 class="capitalLetters">${pokemonData.name}</h1>
-//                     <span>#${pokemonData.id}</span>
-//                 </div>
-//                 <span class="capitalLetters">${pokemonData.types[0].type.name}</span>
-//             </div>
-//         </div>
-//         <div class="infoContainer">
-//             <div class="pokemonImageContainer">
-//                 <img src="${pokemonData.sprites.other['official-artwork'].front_default}" alt="${pokemonData.name}">
-//             </div>
-//             <div class="statsHeader">
-//                 <span>About</span>
-//                 <span>Base Stats</span>
-//                 <span>Evolution</span>
-//                 <span>Moves</span>
-//             </div>
-//             <div class="padding32">
-//                 <div class="statsContent">
-//                     <div class="statsContentProperties">
-//                         <span>Height</span>
-//                         <span>Weight</span>
-//                         <span>Ability 1</span>
-//                         <span>Ability 2</span>
-//                     </div>
-
-//                     <div class="statsContentValues">
-//                         <span>${pokemonData.height}</span>
-//                         <span>${pokemonData.weight}</span>
-//                         <span>${pokemonData.abilities[0].ability.name}</span>
-//                         <span>${pokemonData.abilities[1].ability.name}</span>
-//                     </div>
-//                 </div>
-//                 <h2>Breeding</h2>
-//             </div>
 //         </div>
 //     </div>
 //     `;
